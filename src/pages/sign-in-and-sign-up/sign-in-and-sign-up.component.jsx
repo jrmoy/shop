a72699159a3ddefe5d38494d43f7638e7511386f
Jyotirmoy Ghosh <jrmoyghosh@gmail.com>
2020-06-11 00:57:08 -0500

    Sign in form added

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './sign-in-and-sign-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SigninAndSignUpPage = () => (
+  <div classname='sign-in-and-sign-up'>
+    <SignIn/>
+  </div>
+);
+export default SigninAndSignUpPage;
