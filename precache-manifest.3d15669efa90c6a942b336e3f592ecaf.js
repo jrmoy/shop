00f52e2013e10b2bda236f6fbf3567eaf9c6231a
Jyotirmoy Ghosh <jrmoyghosh@gmail.com>
2020-06-11 01:44:59 -0500

    Updates

---
+++ precache-manifest.3d15669efa90c6a942b336e3f592ecaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f1bb4f3cb9c62ec29a555ed0e7c1f2",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "c12f2b519e5d8a034762",
+    "url": "/shop/static/css/main.4b34a9c9.chunk.css"
+  },
+  {
+    "revision": "c87cc77d5a6be6f98f11",
+    "url": "/shop/static/js/2.121f8604.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shop/static/js/2.121f8604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12f2b519e5d8a034762",
+    "url": "/shop/static/js/main.40edb2f1.chunk.js"
+  },
+  {
+    "revision": "48094c2a79f3bcf59b60",
+    "url": "/shop/static/js/runtime-main.6e7e8b63.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/shop/static/media/crown.ed28accb.svg"
+  }
+]);
